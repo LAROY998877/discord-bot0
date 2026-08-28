c48a110902cbd9f74c3aaf40922b9a873cdcdae3
LAROY998877 <roobn217@gmail.com>
2026-08-28 09:43:16 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,4 +14,4 @@ async def on_ready():
 @bot.command()
 async def test(ctx):
     await ctx.send('Hello! The bot is working 100% 🎯')
-bot.run('MTU0MjgwODk1NDQyNDUyNDg4MA.GOIlX8.WXluu4_BhX1H3P10dHUfWR302dU8RP24C0-TeU')
+bot.run('MTU0MjgwODk1NDQyNDUyNDg4MA.GqJ7tL.y6Kq0_Yy5dc1BLQbqSHrA3SuqGn0vzNOuWL05M')
